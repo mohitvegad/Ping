32be779bf6e9f1e607483e3f8ab9d9f9bc554426
Mohit Vegad <kfdvhtq424@privaterelay.appleid.com>
2026-05-22 22:33:23 +0100

    Added Ping Project!

---
+++ Ping/PingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingApp.swift
+//  Ping
+//
+//  Created by Mohit Vegad on 22/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct PingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
